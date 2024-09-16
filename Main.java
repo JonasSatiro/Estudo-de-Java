85e8d40a4ff013dba8a6ac8ecff17a3b99d7bb1e
JonasSatiro <jonassatiro123@gmail.com>
2024-09-16 20:28:22 -0300

    aula 003

--- Main.java
+++ Main.java
@@ -3,3 +3,9 @@ public class Main {
         System.out.println("Hello, World!");
     }
 }
+
+// public class Main: Define uma classe chamada Main. Em Java, todo código deve estar dentro de uma classe.
+
+// public static void main(String[] args): Esse é o ponto de entrada do programa. Quando você executa o programa Java, o método main é chamado.
+
+// System.out.println("Hello, World!");: Este comando imprime a frase "Hello, World!" no console loosely.
---
+++ aula003.java
@@ -0,0 +1,26 @@
+//declaracao de uma classe publica
+public class aula003{
+    //metodo principal(ponto de entrada)
+    public static void main(String[] args) {
+        //corpo do metodo - onde o codigo e' executado
+        byte bits8 = 8;
+        short bits16 = 16;
+        int bits32 = 32;
+        long bits64 = 64;
+        float bits32FlutuantePrecisao = 32;
+        double peso = 90.1;
+        char umUnicoCaracter = J;
+        
+
+        /*
+        Tipos Primitivos:
+
+            int: Inteiros (ex: int idade = 25;)
+            double: Números de ponto flutuante (ex: double altura = 1.75;)
+            char: Um único caractere (ex: char letra = 'A';)
+            boolean: Verdadeiro ou falso (ex: boolean ativo = true;)
+
+        */
+
+    }
+}
